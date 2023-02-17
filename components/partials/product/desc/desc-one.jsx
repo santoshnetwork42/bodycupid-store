defdb206efc41882668e80ee8db223dffc234ebd
fitandglow <48804810+fitandglow@users.noreply.github.com>
2023-02-18 00:43:43 +0530

    Revert "product detail page fix"
    This reverts commit cdd6b589b4271872e6dc4eacceb377bf35d2eb26.

--- components/partials/product/desc/desc-one.jsx
+++ components/partials/product/desc/desc-one.jsx
@@ -11,13 +11,23 @@ import { toDecimal } from "~/utils";
 function DescOne(props) {
   const { product, isGuide = true, isDivider = true, openModal } = props;
 
-  let sizes = [];
+  let colors = [],
+    sizes = [];
 
   if (product.variants.items.length > 0) {
     if (product.variants.items[0].size)
       product.variants.items.forEach((item) => {
-        sizes.push({ name: item.title, value: item.id });
+        if (sizes.findIndex((size) => size.name === item.size.name) === -1) {
+          sizes.push({ name: item.size.name, value: item.size.size });
+        }
       });
+
+    if (product.variants.items[0].color) {
+      product.variants.items.forEach((item) => {
+        if (colors.findIndex((color) => color.name === item.color.name) === -1)
+          colors.push({ name: item.color.name, value: item.color.color });
+      });
+    }
   }
 
   const setRating = (e) => {
