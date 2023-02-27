76230092942933a205b08cc7b4e7aa729bde6dbb
brijesh <brijesh.agarwal26@gmail.com>
2023-02-27 21:14:03 +0530

    image opt fix

--- utils/optimizeImage.js
+++ utils/optimizeImage.js
@@ -13,7 +13,7 @@ const optimizeImage = async ({ src, type = "url", options = {} }) => {
     originalUrl: src,
   };
 
-  if (["prod", "production"].includes(NODE_ENV) && false) {
+  if (["prod", "production"].includes(NODE_ENV)) {
     try {
       let imageBuffer = null;
 
