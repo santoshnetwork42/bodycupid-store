ea2ec1f2f6931a23faf579feb2b33e2d7f476d0a
chiragmparmar <itschiragparmar@gmail.com>
2023-02-27 12:41:26 +0530

    fix small change

--- components/partials/shop/product-list/product-list-one.jsx
+++ components/partials/shop/product-list/product-list-one.jsx
@@ -179,7 +179,7 @@ function ProductListOne(props) {
       ) : (
         <div className="product-lists product-wrapper">
           {products.map((item) => (
-            <ProductEight products={products} product={item} key={"shop-list-" + item.id} />
+            <ProductEight product={item} key={"shop-list-" + item.id} />
           ))}
         </div>
       )}
