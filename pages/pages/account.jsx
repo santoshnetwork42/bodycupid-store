c9cdfe3f90d5cf12e6d0dbded1fd9079a36187ab
brijesh <brijesh.agarwal26@gmail.com>
2023-03-08 19:10:54 +0530

    account orders

--- pages/pages/account.jsx
+++ pages/pages/account.jsx
@@ -29,7 +29,11 @@ function Account({ user }) {
     } = await API.graphql({
       query: searchOrders,
       variables: {
-        filter: { userId: { eq: user.id }, storeId: { eq: STORE_ID } },
+        filter: {
+          userId: { eq: user.id },
+          storeId: { eq: STORE_ID },
+          status: { eq: "CONFIRMED" },
+        },
         sort: [{ field: "orderDate", direction: "desc" }],
       },
       authMode: "AMAZON_COGNITO_USER_POOLS",
