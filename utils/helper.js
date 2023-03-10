938dc10468074e51331a3cecdd7f8f3ac999f36e
chiragmparmar <itschiragparmar@gmail.com>
2023-03-10 14:10:04 +0530

    fix

--- utils/helper.js
+++ utils/helper.js
@@ -42,3 +42,30 @@ export const deliveryRemainingTime = () => {
     return hh > 0 ? `${hh} hrs ${mm} mins` : `${mm} mins`;
   }
 };
+
+export const removeHoverEffect=()=>{
+  function hasTouch() {
+    return (
+      "ontouchstart" in document.documentElement ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0
+    );
+  }
+
+  if (hasTouch()) {
+    try {
+      for (var si in document.styleSheets) {
+        var styleSheet = document.styleSheets[si];
+        if (!styleSheet.rules) continue;
+
+        for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+          if (!styleSheet.rules[ri].selectorText) continue;
+
+          if (styleSheet.rules[ri].selectorText.match(":hover")) {
+            styleSheet.deleteRule(ri);
+          }
+        }
+      }
+    } catch (ex) {}
+  }
+}
