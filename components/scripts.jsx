5eee86a0ae0820bb78c3b7860927a83791a0a1dc
chiragmparmar <itschiragparmar@gmail.com>
2023-03-14 19:16:49 +0530

    fix facebook pixel add

---
+++ components/scripts.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+import TagManager from "react-gtm-module";
+import { GTM_ID } from "~/config";
+
+export default function Scripts() {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: GTM_ID });
+  }, []);
+
+  return <></>;
+}
