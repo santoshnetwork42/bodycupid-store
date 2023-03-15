728fd9e05453faf8deb27ddb5d7559a54deb35d1
chiragmparmar <itschiragparmar@gmail.com>
2023-03-15 11:16:13 +0530

    Add different banner for mobile view

--- components/partials/home/intro-section.jsx
+++ components/partials/home/intro-section.jsx
@@ -5,21 +5,35 @@ import OptimizedImage from "~/components/features/optimized-image";
 
 function IntroSection({ data }) {
   const { banner } = data;
-
   return (
-    <ALink
-      href="/collections/all"
-      className="banner banner-fixed intro-slide2"
-      style={{ backgroundColor: "#dddee0" }}
-    >
-      <figure>
-        <OptimizedImage
-          optimizedData={banner}
-          loading="eager"
-          alt="Intro Slider"
-        />
-      </figure>
-    </ALink>
+    <>
+      <ALink
+        href="/collections/all"
+        className="banner d-sm-none banner-fixed intro-slide2"  //for desktop size
+        style={{ backgroundColor: "#dddee0" }}
+      >
+        <figure>
+          <OptimizedImage
+            optimizedData={banner}
+            loading="eager"
+            alt="Intro Slider"
+          />
+        </figure>
+      </ALink>
+      <ALink
+        href="/collections/all"
+        className="banner d-none d-sm-show banner-fixed intro-slide2"  //for mobile size
+        style={{ backgroundColor: "#dddee0" }}
+      >
+        <figure>
+          <OptimizedImage
+            optimizedData={banner}
+            loading="eager"
+            alt="Intro Slider"
+          />
+        </figure>
+      </ALink>
+    </>
   );
 }
 
