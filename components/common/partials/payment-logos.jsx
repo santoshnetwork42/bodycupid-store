e9dd5a21f9ec1fd291d4741720e00efe445cf1aa
Divyang <divyangparekh0@gmail.com>
2023-03-15 14:59:30 +0530

    cart calculation and redesign according to wow skin science and coupon modal changes

---
+++ components/common/partials/payment-logos.jsx
@@ -0,0 +1,11 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+function PaymentLogos() {
+  return (
+    <figure className="payment">
+      <img src="/images/payment.png" alt="payment" width="200" height="50" />
+    </figure>
+  );
+}
+
+export default PaymentLogos;
