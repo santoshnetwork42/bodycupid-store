5289cf17ff6f2cc74615b82e1f88c790486c04d5
Divyang <divyangparekh0@gmail.com>
2023-03-20 16:23:37 +0530

    create common components for category data and set banner for category

--- pages/collections/[category].jsx
+++ pages/collections/[category].jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Head from "next/head";
 
-import ShopBanner from "~/components/partials/shop/shop-banner";
-import SidebarFilterOne from "~/components/partials/shop/sidebar/sidebar-filter-one";
-import ProductListOne from "~/components/partials/shop/product-list/product-list-one";
+import Category from "~/components/partials/collections/category";
 
 function Categories() {
   return (
@@ -14,19 +12,7 @@ function Categories() {
 
       <h1 className="d-none">Wow life science - Shop Page</h1>
 
-      <ShopBanner />
-
-      <div className="page-content mb-10 pb-3">
-        <div className="container">
-          <div className="row main-content-wrap gutter-lg">
-            <SidebarFilterOne />
-
-            <div className="col-lg-9 main-content">
-             <ProductListOne />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Category />
     </main>
   );
 }
