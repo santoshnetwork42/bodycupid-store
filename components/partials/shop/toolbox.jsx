2e6ab0fc913a31f0d6a92e27ea23682b05214285
Divyang <divyangparekh0@gmail.com>
2023-03-20 16:34:07 +0530

    remove sort filter code

--- components/partials/shop/toolbox.jsx
+++ components/partials/shop/toolbox.jsx
@@ -156,7 +156,7 @@ export default function ToolBox(props) {
           ) : (
             ""
           )}
-          <div
+          {/* <div
             className={`toolbox-item toolbox-sort ${
               type === "boxed" || type === "banner"
                 ? "select-box text-dark"
@@ -180,7 +180,7 @@ export default function ToolBox(props) {
               <option value="price-low">Price: Low to High</option>
               <option value="price-high">Price: High to Low</option>
             </select>
-          </div>
+          </div> */}
         </div>
 
         <div className="toolbox-right">
