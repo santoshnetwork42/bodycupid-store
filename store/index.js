e2a6055cd4f13b165f265825beaa11cea79aa4ab
Divyang <divyangparekh0@gmail.com>
2023-03-31 14:38:45 +0530

    prevented modal and system store when user logout

--- store/index.js
+++ store/index.js
@@ -1,49 +1,57 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
-import { createWrapper } from 'next-redux-wrapper';
-import createSagaMiddleWare from 'redux-saga';
-import { persistStore } from 'redux-persist';
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { createWrapper } from "next-redux-wrapper";
+import createSagaMiddleWare from "redux-saga";
+import { persistStore } from "redux-persist";
 
-import rootSaga from '~/store/root-saga';
+import rootSaga from "~/store/root-saga";
 
-import cartReducer from '~/store/cart';
-import modalReducer from '~/store/modal';
-import wishlistReducer from '~/store/wishlist';
-import userReducer from '~/store/user';
-import systemReducer from '~/store/system';
+import cartReducer from "~/store/cart";
+import modalReducer from "~/store/modal";
+import wishlistReducer from "~/store/wishlist";
+import userReducer from "~/store/user";
+import systemReducer from "~/store/system";
 
 const actionTypes = {
-    DESTROY_SESSION: 'DESTROY_SESSION'
+  DESTROY_SESSION: "DESTROY_SESSION",
 };
 
 const sagaMiddleware = createSagaMiddleWare();
 
 const appReducer = combineReducers({
-    cart: cartReducer,
-    modal: modalReducer,
-    wishlist: wishlistReducer,
-    user: userReducer,
-    system: systemReducer,
+  cart: cartReducer,
+  modal: modalReducer,
+  wishlist: wishlistReducer,
+  user: userReducer,
+  system: systemReducer,
 });
 
+const preventedStore = ["modal", "system"];
+
 const rootReducers = (state, action) => {
-    // Clear all data in redux store to initial.
-    if (action.type === actionTypes.DESTROY_SESSION) {
-        return appReducer(undefined, action);
-    }
-    return appReducer(state, action);
-}
+  // Clear all data in redux store to initial.
+  if (action.type === actionTypes.DESTROY_SESSION) {
+    const filteredStore = Object.keys(state)
+      .filter((key) => preventedStore.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = state[key];
+        return obj;
+      }, {});
+    return appReducer(filteredStore, action);
+  }
+  return appReducer(state, action);
+};
 
 export const rootActions = {
-    destroySession: () => ({ type: actionTypes.DESTROY_SESSION }),
+  destroySession: () => ({ type: actionTypes.DESTROY_SESSION }),
 };
 
 export const makeStore = (context) => {
-    const store = createStore(rootReducers, applyMiddleware(sagaMiddleware));
+  const store = createStore(rootReducers, applyMiddleware(sagaMiddleware));
 
-    store.sagaTask = sagaMiddleware.run(rootSaga);
-    store.__persistor = persistStore(store);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+  store.__persistor = persistStore(store);
 
-    return store;
+  return store;
 };
 
 export const wrapper = createWrapper(makeStore);
