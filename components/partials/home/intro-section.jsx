54ff698657f1c4ca5c3db5ea592f8a6ddd3d1ad9
Yashua Pervez <yashuapervez@gmail.com>
2023-04-06 14:20:17 +0500

    feat: add homepage icons

--- components/partials/home/intro-section.jsx
+++ components/partials/home/intro-section.jsx
@@ -4,7 +4,7 @@ import ALink from "~/components/features/custom-link";
 import OptimizedImage from "~/components/features/optimized-image";
 
 function IntroSection({ banners }) {
-  const [{ webImage, mobileImage }] = banners || [];
+  const [{ webImage, mobileImage }] = banners || [{}];
 
   return (
     <>
