ee6f934974f628d7a6c07d43553af944770bf3d0
brijesh <brijesh.agarwal26@gmail.com>
2023-04-09 05:34:54 +0530

    optimization fix

--- components/partials/home/intro-section.jsx
+++ components/partials/home/intro-section.jsx
@@ -4,7 +4,7 @@ import ALink from "~/components/features/custom-link";
 import OptimizedImage from "~/components/features/optimized-image";
 
 function IntroSection({ banners }) {
-  const [{ webImage, mobileImage } ={}] = banners || [];
+  const [{ webImage, mobileImage } = {}] = banners || [];
   return (
     <>
       {webImage && (
@@ -16,7 +16,7 @@ function IntroSection({ banners }) {
           <figure>
             <OptimizedImage
               optimizedData={webImage}
-              resizeMobile={false}
+              resize="WIDTH"
               loading="eager"
               alt="Intro Slider"
             />
@@ -32,7 +32,7 @@ function IntroSection({ banners }) {
           <figure>
             <OptimizedImage
               optimizedData={mobileImage}
-              resizeMobile={false}
+              resize="WIDTH"
               loading="eager"
               alt="Intro Slider"
             />
