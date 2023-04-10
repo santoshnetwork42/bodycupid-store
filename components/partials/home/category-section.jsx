dffc26994f682bd0f2d01b5c5eb70714833f6eb4
chiragmparmar <parmarchirag7947@gmail.com>
2023-04-10 16:20:14 +0530

    fix some chnages

--- components/partials/home/category-section.jsx
+++ components/partials/home/category-section.jsx
@@ -1,30 +1,21 @@
 import React from "react";
-import Reveal from "react-awesome-reveal";
 
 import ALink from "~/components/features/custom-link";
 import OptimizedImage from "~/components/features/optimized-image";
 
-import { fadeIn } from "~/utils/data/keyframes";
-
 function CategorySection({ categories = [] }) {
-  const categoriesWithImage = categories.filter((category) => category.image);
-
-  if (categoriesWithImage.length === 0) {
-    return null;
-  }
-
   return (
     <section className="ellipse-section mt-10">
       <div className="container">
         <h2 className="title title-center">Browse Our Categories</h2>
         <div className="row elements">
-          {categoriesWithImage.map((category) => {
+          {categories.map((category) => {
             return (
               <div
                 key={category.id}
-                className="col-xl-3 col col-lg-3 col-md-3 col-sm-4 col-4"
+                className="col-xl-3 col col-lg-3 col-md-2 col-sm-4 col-3"
               >
-                <div className="category category-ellipse">
+                <div className="category category-spacing category-ellipse">
                   <ALink href="#">
                     <figure className="category-media">
                       <OptimizedImage
