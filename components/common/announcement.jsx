c9381311376a6efd2780004169da7028421144db
Divyang <divyangparekh0@gmail.com>
2023-04-10 16:35:52 +0530

    add announcements bar

---
+++ components/common/announcement.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { connect } from "react-redux";
+import { useRouter } from "next/router";
+
+import OwlCarousel from "../features/owl-carousel";
+import { introSlider } from "~/utils/data/carousel";
+
+const Announcement = ({ store }) => {
+  const { announcements = [] } = store;
+  const router = useRouter();
+  const { isReady } = router;
+  return (
+    <>
+      {isReady && (
+        <div className="announcement-bar">
+          <OwlCarousel adClass="owl-nav-bottom" options={introSlider}>
+            {announcements.map((announcement) => {
+              const { id, title } = announcement;
+              return (
+                <div
+                  key={id}
+                  className="announcement testimonial d-flex justify-content-center"
+                >
+                  <p className="announcement-text m-0">{title}</p>
+                </div>
+              );
+            })}
+          </OwlCarousel>
+        </div>
+      )}
+    </>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    store: state.system.store,
+  };
+}
+
+export default connect(mapStateToProps)(Announcement);
