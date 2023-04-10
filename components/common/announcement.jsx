4fddb552e8e534f911b68703852f1daadb88e48c
chiragmparmar <parmarchirag7947@gmail.com>
2023-04-10 17:38:31 +0530

    fix break

--- components/common/announcement.jsx
+++ components/common/announcement.jsx
@@ -6,7 +6,7 @@ import OwlCarousel from "../features/owl-carousel";
 import { announcementSlider } from "~/utils/data/carousel";
 
 const Announcement = ({ store }) => {
-  const { announcements = [] } = store;
+  const { announcements = [] } = store || {};
   const router = useRouter();
   const { isReady } = router;
   return (
