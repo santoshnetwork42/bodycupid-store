1a64e579ad86d63edcfaea8d4d8c2954feac4f6b
Divyang <divyangparekh0@gmail.com>
2023-04-27 16:57:50 +0530

    fix

--- pages/pages/checkout.jsx
+++ pages/pages/checkout.jsx
@@ -187,15 +187,17 @@ function Checkout(props) {
   const placeOrder = useCallback(
     async (e) => {
       e.preventDefault();
+
       if (payMethod === "NONE") {
         toast(
           <AlertPopup
-            message={"please select payment method"}
-            status="warning"
+            message={"Please select payment method"}
+            status="transparent"
           />
         );
         return;
       }
+
       setLoading(true);
 
       const paymentType = isFirst ? "PREPAID" : "COD";
@@ -680,7 +682,7 @@ function Checkout(props) {
                             View details
                           </ALink>
                         </div>
-                        {!shippingAddress?.address || payMethod === "NONE" ? (
+                        {!shippingAddress?.address && payMethod === "NONE" && (
                           <button
                             onClick={() => setAddressModal(true)}
                             className="btn btn-primary btn-rounded btn-order d-flex justify-content-center align-items-center"
@@ -688,7 +690,8 @@ function Checkout(props) {
                             Add new address
                             {loading && <div className="spin-loader ml-2" />}
                           </button>
-                        ) : (
+                        )}
+                        {!!shippingAddress?.address && (
                           <button
                             onClick={placeOrder}
                             className="btn btn-primary btn-rounded btn-order d-flex justify-content-center align-items-center"
