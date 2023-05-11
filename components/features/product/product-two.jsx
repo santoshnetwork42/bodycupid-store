f4a33cf473d488898b1347a2943ee630c0fbac8a
Divyang <divyangparekh0@gmail.com>
2023-05-11 10:54:06 +0530

    fix

--- components/features/product/product-two.jsx
+++ components/features/product/product-two.jsx
@@ -2,7 +2,7 @@ import React, { useMemo } from "react";
 import { connect } from "react-redux";
 
 import ALink from "~/components/features/custom-link";
-import { Star, MagnifyingGlass, Eye } from "~/components/icons";
+import { Star, Eye } from "~/components/icons";
 import { cartActions } from "~/store/cart";
 import { modalActions } from "~/store/modal";
 import { wishlistActions } from "~/store/wishlist";
