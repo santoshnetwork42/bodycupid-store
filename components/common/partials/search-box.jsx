338c383401eca102525f848313b8c8d19b6fccb0
Divyang <divyangparekh0@gmail.com>
2023-05-13 13:09:02 +0530

    search close

--- components/common/partials/search-box.jsx
+++ components/common/partials/search-box.jsx
@@ -124,6 +124,12 @@ function SearchForm({ type = "input", defaultSearch = "" }) {
       pathname: "/collections/search",
       query: { search },
     });
+
+    document.querySelector(".header-search.show-results") &&
+      document
+        .querySelector(".header-search.show-results")
+        .classList.remove("show-results");
+
     return false;
   }
 
