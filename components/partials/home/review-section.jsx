8214766ca15fc7cfc4c1c1b9cf6d31f1a338d6c9
Divyang <divyangparekh0@gmail.com>
2023-05-15 19:02:07 +0530

    add Testimonial

--- components/partials/home/review-section.jsx
+++ components/partials/home/review-section.jsx
@@ -2,80 +2,37 @@ import React from "react";
 import OwlCarousel from "~/components/features/owl-carousel";
 import { mainSlider4 } from "~/utils/data/carousel";
 import RatingStar from "../product/rating-star";
+import { TESTIMONIAL } from "~/constant";
 
 export default function ReviewSection() {
   return (
     <section className="parallax pb-3">
       <div className="container ">
         <OwlCarousel adClass="owl-theme" options={mainSlider4}>
-          <div className="testimonial testimonial-centered testimonial-bg">
-            <div className="testimonial-info">
-              <figure className="testimonial-author-thumbnail">
-                <img
-                  src="/images/agents/4.jpg"
-                  alt="user"
-                  width="50"
-                  height="50"
-                />
-              </figure>
-              <div className="d-flex align-item-center mb-2 justify-content-center">
-                <RatingStar key='rating-star' value={5} />
+          {TESTIMONIAL.map((testimonial) => (
+            <div
+              className="testimonial testimonial-centered testimonial-bg"
+              key={testimonial.says.substring(0, 10)}
+            >
+              <div className="testimonial-info">
+                <figure className="testimonial-author-thumbnail">
+                  <img
+                    src={testimonial.userImage}
+                    alt="user"
+                    width="50"
+                    height="50"
+                  />
+                </figure>
+                <div className="d-flex align-item-center mb-2 justify-content-center">
+                  <RatingStar key="rating-star" value={5} />
+                </div>
+                <blockquote>{testimonial.says}</blockquote>
+                <cite>
+                  {testimonial.name} <span>Customer</span>
+                </cite>
               </div>
-              <blockquote>
-                “This is the best plant store I have ever ordered from! My
-                plants were shipped/delivered so fast. When I had a question,
-                Bloomscape got back to me right away, and they were so
-                friendly!”
-              </blockquote>
-              <cite>
-                Dilshad Khan <span>Customer</span>
-              </cite>
             </div>
-          </div>
-          <div className="testimonial testimonial-centered testimonial-bg">
-            <div className="testimonial-info">
-              <figure className="testimonial-author-thumbnail">
-                <img
-                  src="/images/agents/5.jpg"
-                  alt="user"
-                  width="50"
-                  height="50"
-                />
-              </figure>
-              <blockquote>
-                “ Donec nec justo eget felis facilisis fermentum. Aliquam
-                porttitor mauris sit amet orci. Aenean dignissim pellentesque
-                felis. Morbi in sem quis dui placerat ornare. Pellentesque odio
-                nisi, euismod in, pharetra ”
-              </blockquote>
-              <cite>
-                Herman Beck
-                <span>Investor</span>
-              </cite>
-            </div>
-          </div>
-          <div className="testimonial testimonial-centered testimonial-bg">
-            <div className="testimonial-info">
-              <figure className="testimonial-author-thumbnail">
-                <img
-                  src="/images/agents/2.jpg"
-                  alt="user"
-                  width="50"
-                  height="50"
-                />
-              </figure>
-              <blockquote>
-                “ Donec nec justo eget felis facilisis fermentum. Aliquam
-                porttitor mauris sit amet orci. Aenean dignissim pellentesque
-                felis. Morbi in sem quis dui placerat ornare. Pellentesque odio
-                nisi, euismod in, pharetra ”
-              </blockquote>
-              <cite>
-                Herman Beck
-                <span>Investor</span>
-              </cite>
-            </div>
-          </div>
+          ))}
         </OwlCarousel>
       </div>
     </section>
--- constant.js
+++ constant.js
@@ -6,3 +6,26 @@ export const EMAIl_REGEX =
 export const MAX_PREPAID_DISCOUNT = 500;
 
 export const PRODUCT_TAG_LIST = ["best-seller", "featured", "trending"];
+
+export const TESTIMONIAL = [
+  {
+    name: "Himani",
+    says: "I have been using Body Cupid's Rose Oud Perfume for the past few months, The scent is feminine and romantic, but not overpowering. I've received so many compliments on it since I started wearing it. It's also long-lasting.",
+    userImage: "/images/agents/Himani.jpg",
+  },
+  {
+    name: "Nisha",
+    says: "Body Cupid body mist is my new go-to fragrance for everyday wear. It has a light and refreshing scent that's perfect for a quick spritz before heading out the door. I love how affordable it is, too!",
+    userImage: "/images/agents/Nisha.jpg",
+  },
+  {
+    name: "Anamika",
+    says: "Body Cupid Scandal perfume is simply amazing! The scent is seductive and mysterious. I also love the gorgeous bottle it comes in, which looks beautiful on my dresser.",
+    userImage: "/images/agents/Anamika.jpg",
+  },
+  {
+    name: "Anusha",
+    says: "I have tried many skincare brands in the past, but nothing compares to Body Cupid's shower gel. I love how the product makes It lathers up nicely and leaves my skin feeling clean and refreshed.",
+    userImage: "/images/agents/Anusha.jpg",
+  },
+];
