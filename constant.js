4c9661fc6ab79d043ca3163affcfc577a4cc146f
Divyang <divyangparekh0@gmail.com>
2023-05-15 14:53:37 +0530

    Show only best seller, featured, treding as label on products /PLP

--- constant.js
+++ constant.js
@@ -4,3 +4,5 @@ export const EMAIl_REGEX =
   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 export const MAX_PREPAID_DISCOUNT = 500;
+
+export const PRODUCT_TAG_LIST = ["best-seller", "featured", "trending"];
