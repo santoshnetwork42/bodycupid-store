db63f6fc325b0e2b0066eb95a81541ad7a055837
Divyang <divyangparekh0@gmail.com>
2023-05-15 19:41:59 +0530

    replace with next image

--- components/partials/home/review-section.jsx
+++ components/partials/home/review-section.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import OwlCarousel from "~/components/features/owl-carousel";
 import { mainSlider4 } from "~/utils/data/carousel";
 import RatingStar from "../product/rating-star";
-import { TESTIMONIAL } from "~/constant";
+import Image from "next/image";
 
+const TESTIMONIAL = [
+  {
+    name: "Himani",
+    says: "I have been using Body Cupid's Rose Oud Perfume for the past few months, The scent is feminine and romantic, but not overpowering. I've received so many compliments on it since I started wearing it. It's also long-lasting.",
+    userImage: "/images/agents/Himani.jpg",
+  },
+  {
+    name: "Nisha",
+    says: "Body Cupid body mist is my new go-to fragrance for everyday wear. It has a light and refreshing scent that's perfect for a quick spritz before heading out the door. I love how affordable it is, too!",
+    userImage: "/images/agents/Nisha.jpg",
+  },
+  {
+    name: "Anamika",
+    says: "Body Cupid Scandal perfume is simply amazing! The scent is seductive and mysterious. I also love the gorgeous bottle it comes in, which looks beautiful on my dresser.",
+    userImage: "/images/agents/Anamika.jpg",
+  },
+  {
+    name: "Anusha",
+    says: "I have tried many skincare brands in the past, but nothing compares to Body Cupid's shower gel. I love how the product makes It lathers up nicely and leaves my skin feeling clean and refreshed.",
+    userImage: "/images/agents/Anusha.jpg",
+  },
+];
 export default function ReviewSection() {
   return (
     <section className="parallax pb-3">
@@ -16,11 +38,11 @@ export default function ReviewSection() {
             >
               <div className="testimonial-info">
                 <figure className="testimonial-author-thumbnail">
-                  <img
+                  <Image
                     src={testimonial.userImage}
                     alt="user"
-                    width="50"
-                    height="50"
+                    width="70"
+                    height="70"
                   />
                 </figure>
                 <div className="d-flex align-item-center mb-2 justify-content-center">
