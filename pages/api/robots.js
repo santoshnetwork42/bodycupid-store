523f52d06db57fbd0c0fdb01199c4f2eb85574c0
brijesh <brijesh.agarwal26@gmail.com>
2023-05-18 16:07:12 +0530

    adds robots.txt

--- pages/api/robots.js
+++ pages/api/robots.js
@@ -1,9 +1,8 @@
-const { NODE_ENV } = process.env;
+const { STORE_ENV } = process.env;
 
 export default async function Revalidate(req, res) {
-  if (NODE_ENV !== "production") {
+  if (STORE_ENV !== "production") {
     const content = ["User-agent: *", "Disallow: /"].join("\n");
-
     res.send(content);
   } else {
     const content = ["User-agent: *", "Allow: /"].join("\n");
