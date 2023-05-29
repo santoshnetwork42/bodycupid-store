19a249b4b7cb98f31ed1471995d3a129a5dfa4f6
chiragmparmar <parmarchirag7947@gmail.com>
2023-05-29 11:57:40 +0530

    made changes

--- components/common/partials/passwordless.jsx
+++ components/common/partials/passwordless.jsx
@@ -244,8 +244,7 @@ function Passwordless({
                                 <div className="prefix">+91</div>
                                 <input
                                   type="tel"
-                                  className="form-control prevent-zoom"
-                                  
+                                  className="form-control prevent-zoom"            
                                   id="singin-phone-2"
                                   name="singin-phone"
                                   placeholder="Phone number *"
@@ -257,7 +256,7 @@ function Passwordless({
                                       ...state,
                                       phone: (e.target.value || "")
                                         ?.replaceAll(/[^0-9]+/g, "")
-                                        ?.trim(),
+                                        ?.trim(), 
                                     });
                                   }}
                                 />
