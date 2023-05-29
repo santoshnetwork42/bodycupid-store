639baeab7a29653a139448488366c57e7d1dec06
chirag-devx <chirag@devxconsultancy.com>
2023-05-29 11:29:56 +0530

    fix  ios zooming issue

--- components/common/partials/passwordless.jsx
+++ components/common/partials/passwordless.jsx
@@ -244,7 +244,7 @@ function Passwordless({
                                 <div className="prefix">+91</div>
                                 <input
                                   type="tel"
-                                  className="form-control"
+                                  className="form-control prevent-zoom"
                                   id="singin-phone-2"
                                   name="singin-phone"
                                   placeholder="Phone number *"
