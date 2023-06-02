68a82b5cf70130785eb16a8677ff46ab13a09c92
chiragmparmar <parmarchirag7947@gmail.com>
2023-06-02 11:17:37 +0530

    notify me button made sticky

--- components/features/product-notify.jsx
+++ components/features/product-notify.jsx
@@ -94,7 +94,7 @@ function ProductNotify(props) {
           <div className="cart-button-wrapper">
           <button
             className="notify-btn btn
-            btn-block btn-rounded d-flex justify-content-center align-items-center text-capitalize font-weight-semi-bold mt-3"
+            btn-block btn-rounded d-flex justify-content-center align-items-center text-capitalize font-weight-semi-bold mt-1"
             type="submit"
             disabled={loading}
             
