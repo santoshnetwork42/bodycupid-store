068a1da00ad457d85790b3711cc700769b4b8c7d
brijesh <brijesh.agarwal26@gmail.com>
2023-06-07 13:07:30 +0530

    customer glu

---
+++ components/scripts/cutomer-glu.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Script from "next/script";
+
+function CustomerGlu() {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `window["gluConfig"] = {
+            writeKey: "6dc42e8449e1144c4fe8583568cbbf6a44270f9c",  //provided by CustomerGlu
+            userIdentification: {
+              userId: null
+            },
+            userAttributes: {
+              gluAttributes: {},        //add CG reserved user properties here (key-value pair)
+              customAttributes: {}      //add all your custom user properties here (key-value pair) 
+            },
+            onLoadError: function () {  //error handling function if SDK fails to load
+              console.log("error"); 
+            }
+          }`,
+        }}
+      />
+
+      <Script
+        strategy="afterInteractive"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (()=>{"use strict";!function(){const e=window.gluConfig||{},t=[],o={open:{returnPromise:!0,callMethod:"open"},close:{returnPromise:!1,callMethod:"close"},register:{returnPromise:!1,callMethod:"register"},listenToAnalytics:{returnPromise:!1,callMethod:"listenToAnalytics"},getCampaignDetails:{returnPromise:!0,callMethod:"getCampaignDetails"}},r=["open","close","register","listenToAnalytics","getCampaignDetails"],n={};for(let e=0;e<r.length;e++)n[r[e]]=function(){const n=o[r[e]],s=Array.prototype.slice.call(arguments);if(n.returnPromise)return new Promise(((e,o)=>{t.push({callMethod:n.callMethod,arguments:s.concat({resolve:e,reject:o}),isPromise:!0})}));t.push({callMethod:n.callMethod,arguments:s})};let s=document.createElement("script");s.type="text/javascript",s.async=!0,s.src="https://assets.customerglu.com/scripts/sdk/v4.6/sdk.js",s.onload=function(){const o=new window.CustomerGlu(e.writeKey,e.userIdentification,e.userAttributes);for(let e=0;e<t.length;e++)o[t[e].callMethod](...t[e].arguments);window.glu=o},s.onerror=function(){if(t&&t.length){for(let e=0;e<t.length;e++)t[e].isPromise&&t[e].arguments[t[e].arguments.length-1]("GLU_SDK_LOAD_ERROR");e.onLoadError&&e.onLoadError()}},document.getElementsByTagName("body")[0].appendChild(s),window.glu=n}()})();
+      `,
+        }}
+      />
+    </>
+  );
+}
+
+export default React.memo(CustomerGlu);
