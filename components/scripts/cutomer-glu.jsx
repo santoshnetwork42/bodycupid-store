e598f49bf7d1a759548babe11080a827a36e2257
brijesh <brijesh.agarwal26@gmail.com>
2023-06-13 13:45:59 +0530

    fix

--- components/scripts/cutomer-glu.jsx
+++ components/scripts/cutomer-glu.jsx
@@ -4,23 +4,49 @@ import { connect } from "react-redux";
 import awaitGlobal from "await-global";
 
 import { CUSTOMER_GLU_KEY } from "~/config";
+import { modalActions } from "~/store/modal";
 
-function CustomerGlu({ user }) {
+function CustomerGlu({ user, openLogin }) {
   useEffect(() => {
-    (async function () {
-      if (user) {
-        const glu = await awaitGlobal("glu");
+    let isScriptLoaded = false;
+
+    const trigger = () => {
+      awaitGlobal("glu").then((glu) => {
         if (glu) {
-          glu.register(CUSTOMER_GLU_KEY, { userId: user.id }, {});
+          if (user) {
+            glu.register(CUSTOMER_GLU_KEY, { userId: user.id }, {});
+          }
+
+          if (glu.addAnalyticsListener) {
+            isScriptLoaded = true;
+            glu.addAnalyticsListener((event) => {
+              if (
+                event?.event_name === "BUTTON_CLICKED" &&
+                event?.interaction_details?.button_name === "Login To Play"
+              ) {
+                openLogin(false);
+                glu.closeAllOverlays();
+              }
+            });
+          }
         }
-      }
-    })();
+      });
+    };
+
+    const intervalId = setInterval(() => {
+      if (!isScriptLoaded) trigger();
+      else clearInterval(intervalId);
+    }, 500);
+
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
   }, [user]);
 
   return (
     <>
       <Script
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         type="text/javascript"
         dangerouslySetInnerHTML={{
           __html: `window["gluConfig"] = {
@@ -40,11 +66,11 @@ function CustomerGlu({ user }) {
       />
 
       <Script
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         type="text/javascript"
         dangerouslySetInnerHTML={{
           __html: `
-            (()=>{"use strict";!function(){const e=window.gluConfig||{},t=[],o={open:{returnPromise:!0,callMethod:"open"},close:{returnPromise:!1,callMethod:"close"},register:{returnPromise:!1,callMethod:"register"},listenToAnalytics:{returnPromise:!1,callMethod:"listenToAnalytics"},getCampaignDetails:{returnPromise:!0,callMethod:"getCampaignDetails"}},r=["open","close","register","listenToAnalytics","getCampaignDetails"],n={};for(let e=0;e<r.length;e++)n[r[e]]=function(){const n=o[r[e]],s=Array.prototype.slice.call(arguments);if(n.returnPromise)return new Promise(((e,o)=>{t.push({callMethod:n.callMethod,arguments:s.concat({resolve:e,reject:o}),isPromise:!0})}));t.push({callMethod:n.callMethod,arguments:s})};let s=document.createElement("script");s.type="text/javascript",s.async=!0,s.src="https://assets.customerglu.com/scripts/sdk/v4.6/sdk.js",s.onload=function(){const o=new window.CustomerGlu(e.writeKey,e.userIdentification,e.userAttributes);for(let e=0;e<t.length;e++)o[t[e].callMethod](...t[e].arguments);window.glu=o},s.onerror=function(){if(t&&t.length){for(let e=0;e<t.length;e++)t[e].isPromise&&t[e].arguments[t[e].arguments.length-1]("GLU_SDK_LOAD_ERROR");e.onLoadError&&e.onLoadError()}},document.getElementsByTagName("body")[0].appendChild(s),window.glu=n}()})();
+            (()=>{"use strict";!function(){const e=window.gluConfig||{},t=[],o={open:{returnPromise:!0,callMethod:"open"},close:{returnPromise:!1,callMethod:"close"},register:{returnPromise:!1,callMethod:"register"},listenToAnalytics:{returnPromise:!1,callMethod:"listenToAnalytics"},getCampaignDetails:{returnPromise:!0,callMethod:"getCampaignDetails"}},r=["open","close","register","listenToAnalytics","getCampaignDetails"],n={};for(let e=0;e<r.length;e++)n[r[e]]=function(){const n=o[r[e]],s=Array.prototype.slice.call(arguments);if(n.returnPromise)return new Promise(((e,o)=>{t.push({callMethod:n.callMethod,arguments:s.concat({resolve:e,reject:o}),isPromise:!0})}));t.push({callMethod:n.callMethod,arguments:s})};let s=document.createElement("script");s.type="text/javascript",s.async=!0,s.src="https://assets.customerglu.com/scripts/sdk/v4.7/sdk.js",s.onload=function(){const o=new window.CustomerGlu(e.writeKey,e.userIdentification,e.userAttributes);for(let e=0;e<t.length;e++)o[t[e].callMethod](...t[e].arguments);window.glu=o},s.onerror=function(){if(t&&t.length){for(let e=0;e<t.length;e++)t[e].isPromise&&t[e].arguments[t[e].arguments.length-1]("GLU_SDK_LOAD_ERROR");e.onLoadError&&e.onLoadError()}},document.getElementsByTagName("body")[0].appendChild(s),window.glu=n}()})();
       `,
         }}
       />
@@ -58,4 +84,6 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(CustomerGlu);
+export default connect(mapStateToProps, {
+  openLogin: modalActions.openPasswordlessModal,
+})(CustomerGlu);
