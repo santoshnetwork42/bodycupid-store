cc14d835b9c8cf6ce089adeffc91a5d54ade3242
chiragmparmar <parmarchirag7947@gmail.com>
2023-06-17 13:51:54 +0530

    added buy x get y coupon banner

--- components/common/couponDiscountBar.js
+++ components/common/couponDiscountBar.js
@@ -5,7 +5,6 @@ import { useFeaturedCoupons } from "~/utils/hooks/useCoupon";
 const couponDiscountBar = (props) => {
   const { cartList } = props;
   const featuredCoupons = useFeaturedCoupons();
-  console.log(featuredCoupons)
   const bxgyCoupon = featuredCoupons.find((coupon) => coupon.couponType === "BUY_X_GET_Y");
 
   const couponText = !bxgyCoupon?.allowed
