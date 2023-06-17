3945c2ad2a17ded5b92156c4ffaf6c8fa8eac50a
chiragmparmar <parmarchirag7947@gmail.com>
2023-06-17 11:42:18 +0530

    added buy 1 get 1 coupon banner

--- components/common/couponDiscountBar.js
+++ components/common/couponDiscountBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { useFeaturedCoupons } from "~/utils/hooks/useCoupon";
 
@@ -6,22 +6,30 @@ const CouponDiscountBar = (props) => {
   const { cartList, couponBanner } = props;
   const featuredCoupons = useFeaturedCoupons();
   const b1g1Coupon = featuredCoupons.find((coupon) => coupon.code === "B1G1");
+  const [showBar, setShowBar] = useState(false);
+  const [text, setText] = useState("");
 
-  if (couponBanner && !!cartList.length && b1g1Coupon) {
-    return (
-      <div className="coupon-discount-bar">
+  useEffect(() => {
+    if (couponBanner && cartList.length > 0 && b1g1Coupon) {
+      setShowBar(true);
+      setText(!b1g1Coupon?.allowed ? "Add 1 more item to unlock 'Buy 1 get 1 Offer'" : "Congrats! You have unlocked B1G1 Offer");
+    } else {
+      setShowBar(false);
+      setText("");
+    }
+  }, [couponBanner, cartList, b1g1Coupon]);
+
+  return (
+    <div className={`coupon-discount-bar ${showBar ? "show" : ""}`}>
+      {showBar && (
         <div className="coupon-discount">
-          <p className="coupon-discount-text font-weight-semi-bold pt-1 pb-1 m-0">
-            {!b1g1Coupon.allowed
-              ? "Add 1 more item to unlock 'Buy 1 get 1 Offer'"
-              : "Congrats! You have unlocked B1G1 Offer"}
+          <p className={`coupon-discount-text font-weight-semi-bold pt-1 pb-1 m-0 ${text === "Congrats! You have unlocked B1G1 Offer" ? "animate" : ""}`}>
+            {text}
           </p>
         </div>
-      </div>
-    );
-  }
-  
-  return <></>;
+      )}
+    </div>
+  );
 };
 
 function mapStateToProps(state) {
