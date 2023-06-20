1a71dfdecd892509e683802ed626de7b00e6efbf
Nishita Pandya <nishitapandya@Nishitas-MacBook-Air.local>
2023-06-20 16:02:49 +0530

    added freebie banner

--- components/common/couponDiscountBar.js
+++ components/common/couponDiscountBar.js
@@ -1,32 +1,41 @@
 import React from "react";
 import { connect } from "react-redux";
-import { useFeaturedCoupons } from "~/utils/hooks/useCoupon";
+import { useFeaturedCoupons, useProductCoupons, useFreeProducts } from "~/utils/hooks/useCoupon";
+import { GiftBox } from "../icons";
 
 const couponDiscountBar = (props) => {
   const { cartList } = props;
   const featuredCoupons = useFeaturedCoupons();
-  
+  const freeProductsResponse = useFreeProducts(true);
   const bxgyCoupon = featuredCoupons.find((coupon) => coupon.couponType === "BUY_X_GET_Y");
 
-  const couponText = !bxgyCoupon?.allowed
-    ? `Add more items to unlock 'Buy ${bxgyCoupon?.buyXQuantity} get ${bxgyCoupon?.getYQuantity} Offer'`
-    : `Congrats, your free product is added to cart!`;
+  let couponText = "";
 
-    return (
+  if (bxgyCoupon) {
+    couponText = bxgyCoupon.allowed ?
+      `Congrats, your free product is added to cart!` : `Add more items to unlock 'Buy ${bxgyCoupon.buyXQuantity} get ${bxgyCoupon.getYQuantity} Offer'`;  
+  } else if (freeProductsResponse[0]?.allowed) {
+    couponText = (
       <>
-        {!!cartList.length && !!bxgyCoupon && (
-          <div className="coupon-discount-bar ">
-            <div className="coupon-discount">
-            <p className={`coupon-discount-text font-weight-semi-bold pt-1 pb-1 m-0 
-              ${bxgyCoupon?.allowed ? "animate" : ""}`}>
-              {couponText}
-              </p>
-            </div>
-          </div>
-        )
-        }
+       <GiftBox />
+       A surprise is added to your cart!
       </>
     );
+  }
+
+  return (
+    <>
+      {!!cartList.length && (!!bxgyCoupon || freeProductsResponse[0]?.allowed) && (
+        <div className="coupon-discount-bar">
+          <div className="coupon-discount">
+            <p className={`coupon-discount-text font-weight-semi-bold pt-1 pb-1 m-0 ${bxgyCoupon?.allowed ? "animate" : ""}`}>
+              {couponText}
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 function mapStateToProps(state) {
