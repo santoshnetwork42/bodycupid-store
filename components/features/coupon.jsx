ad1c5cd4b49c7abb9519dae9f7b466f05fd59b31
Nishita Pandya <nishitapandya@Nishitas-MacBook-Air.local>
2023-06-20 13:03:17 +0530

    fixed free products in cart being stacked bug

--- components/features/coupon.jsx
+++ components/features/coupon.jsx
@@ -106,7 +106,6 @@ function Coupon(props) {
               cartItemSource: "COUPON",
             });
           }
-
           logger.info("Applied coupon:", response);
         } else {
           setError(message);
