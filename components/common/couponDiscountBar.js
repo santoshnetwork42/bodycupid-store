18026ca7e964f1af6c32ad3cce3ef24264bb01ac
Brijesh Agarwal <brijesh.agarwal26@gmail.com>
2023-06-21 13:00:09 +0530

    lint

--- components/common/couponDiscountBar.js
+++ components/common/couponDiscountBar.js
@@ -6,36 +6,43 @@ import { GiftBox } from "~/components/icons";
 const CouponBanner = (message) => (
   <div className="coupon-discount-bar">
     <div className="coupon-discount">
-      <p className={`coupon-discount-text font-weight-semi-bold pt-1 pb-1 m-0 ${bxgyCoupon?.allowed ? "animate" : ""}`}>
+      <p
+        className={`coupon-discount-text font-weight-semi-bold pt-1 pb-1 m-0 ${
+          bxgyCoupon?.allowed ? "animate" : ""
+        }`}
+      >
         {message}
       </p>
     </div>
   </div>
-)
+);
 
 const couponDiscountBar = () => {
   const featuredCoupons = useFeaturedCoupons();
   const freeProductsResponse = useFreeProducts(true);
-  const bxgyCoupon = featuredCoupons.find((coupon) => coupon.couponType === "BUY_X_GET_Y" && coupon.autoApply);
+  const bxgyCoupon = featuredCoupons.find(
+    (coupon) => coupon.couponType === "BUY_X_GET_Y" && coupon.autoApply
+  );
   const [freeProduct] = freeProductsResponse;
 
   if (bxgyCoupon) {
-    const couponText = bxgyCoupon.allowed ?
-      `Congrats, your free product is added to cart!` : `Add more items to unlock 'Buy ${bxgyCoupon.buyXQuantity} get ${bxgyCoupon.getYQuantity} Offer'`;
-    return <CouponBanner message={couponText} />
+    const couponText = bxgyCoupon.allowed
+      ? `Congrats, your free product is added to cart!`
+      : `Add more items to unlock 'Buy ${bxgyCoupon.buyXQuantity} get ${bxgyCoupon.getYQuantity} Offer'`;
+    return <CouponBanner message={couponText} />;
   } else if (freeProduct?.allowed) {
     return (
-      <CouponBanner message={(
-        <>
-          <GiftBox />
-          A surprise is added to your cart!
-        </>
-      )}
+      <CouponBanner
+        message={
+          <>
+            <GiftBox />A surprise is added to your cart!
+          </>
+        }
       />
     );
   }
 
-  return <></>
+  return <></>;
 };
 
 export default couponDiscountBar;
