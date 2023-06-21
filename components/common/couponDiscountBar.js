b4cd96fcf742eb932f1505f836d3b3d7d0c9ed45
Brijesh Agarwal <48677223+brijesh2609@users.noreply.github.com>
2023-06-21 07:29:09 +0000

    coupon discount bar messaging

--- components/common/couponDiscountBar.js
+++ components/common/couponDiscountBar.js
@@ -1,47 +1,41 @@
 import React from "react";
-import { connect } from "react-redux";
-import { useFeaturedCoupons, useProductCoupons, useFreeProducts } from "~/utils/hooks/useCoupon";
-import { GiftBox } from "../icons";
 
-const couponDiscountBar = (props) => {
-  const { cartList } = props;
+import { useFeaturedCoupons, useFreeProducts } from "~/utils/hooks/useCoupon";
+import { GiftBox } from "~/components/icons";
+
+const CouponBanner = (message) => (
+  <div className="coupon-discount-bar">
+    <div className="coupon-discount">
+      <p className={`coupon-discount-text font-weight-semi-bold pt-1 pb-1 m-0 ${bxgyCoupon?.allowed ? "animate" : ""}`}>
+        {message}
+      </p>
+    </div>
+  </div>
+)
+
+const couponDiscountBar = () => {
   const featuredCoupons = useFeaturedCoupons();
   const freeProductsResponse = useFreeProducts(true);
-  const bxgyCoupon = featuredCoupons.find((coupon) => coupon.couponType === "BUY_X_GET_Y");
-
-  let couponText = "";
+  const bxgyCoupon = featuredCoupons.find((coupon) => coupon.couponType === "BUY_X_GET_Y" && coupon.autoApply);
+  const [freeProduct] = freeProductsResponse;
 
   if (bxgyCoupon) {
-    couponText = bxgyCoupon.allowed ?
-      `Congrats, your free product is added to cart!` : `Add more items to unlock 'Buy ${bxgyCoupon.buyXQuantity} get ${bxgyCoupon.getYQuantity} Offer'`;  
-  } else if (freeProductsResponse[0]?.allowed) {
-    couponText = (
-      <>
-       <GiftBox />
-       A surprise is added to your cart!
-      </>
+    const couponText = bxgyCoupon.allowed ?
+      `Congrats, your free product is added to cart!` : `Add more items to unlock 'Buy ${bxgyCoupon.buyXQuantity} get ${bxgyCoupon.getYQuantity} Offer'`;
+    return <CouponBanner message={couponText} />
+  } else if (freeProduct?.allowed) {
+    return (
+      <CouponBanner message={(
+        <>
+          <GiftBox />
+          A surprise is added to your cart!
+        </>
+      )}
+      />
     );
   }
 
-  return (
-    <>
-      {!!cartList.length && (!!bxgyCoupon || freeProductsResponse[0]?.allowed) && (
-        <div className="coupon-discount-bar">
-          <div className="coupon-discount">
-            <p className={`coupon-discount-text font-weight-semi-bold pt-1 pb-1 m-0 ${bxgyCoupon?.allowed ? "animate" : ""}`}>
-              {couponText}
-            </p>
-          </div>
-        </div>
-      )}
-    </>
-  );
+  return <></>
 };
 
-function mapStateToProps(state) {
-  return {
-    cartList: state.cart.data ? state.cart.data : [],
-  };
-}
-
-export default connect(mapStateToProps)(couponDiscountBar);
+export default couponDiscountBar;
