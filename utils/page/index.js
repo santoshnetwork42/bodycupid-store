2a3a6300b3aeb0b0e314963c3746ce216af57d96
Brijesh Agarwal <brijesh.agarwal26@gmail.com>
2023-06-23 15:30:55 +0530

    fix

--- utils/page/index.js
+++ utils/page/index.js
@@ -1,6 +1,3 @@
-import React from "react";
-import dynamic from "next/dynamic";
-
 import { getPublicImageURL } from "~/utils/getPublicImageUrl";
 import fetchData from "~/utils/fetchData";
 import { STORE_ID } from "~/config";
@@ -10,83 +7,6 @@ import {
   findProducts,
   getStoreBanners,
 } from "~/graphql/api";
-
-import NextHead from "~/components/common/next-head";
-import IntroSection from "~/components/partials/home/intro-section";
-// import StorySection from "~/components/partials/home/story-section";
-
-const CategorySection = dynamic(() =>
-  import("~/components/partials/home/category-section")
-);
-const BrandSection = dynamic(() =>
-  import("~/components/partials/home/brand-section")
-);
-const ReviewSection = dynamic(() =>
-  import("~/components/partials/home/review-section")
-);
-const ProductCollection = dynamic(() =>
-  import("~/components/partials/home/product-collection")
-);
-
-function V1Page({
-  hero,
-  bestSellerProducts,
-  featuredProducts,
-  categories,
-  // storyCategories,
-  brands,
-  store,
-  pageMeta,
-}) {
-  const { name } = store || {};
-
-  return (
-    <main className="main home searchBar">
-      <NextHead {...pageMeta} />
-
-      <h1 className="d-none">{name} - V1Page</h1>
-      <div className="page-content page-content-wrapper product-collection-large">
-        <div className="intro-section">
-          {/* <StorySection categories={storyCategories} /> */}
-          <IntroSection {...hero} />
-          <script
-            async
-            type="text/javascript"
-            src="//asset.fwcdn3.com/js/embed-feed.js"
-          ></script>
-          <fw-embed-feed
-            channel="body_cupid"
-            playlist="gYeK2g"
-            mode="row"
-            open_in="default"
-            max_videos="0"
-            placement="middle"
-            player_placement="bottom-right"
-          ></fw-embed-feed>
-        </div>
-
-        <ProductCollection
-          products={bestSellerProducts}
-          title="Best sellers"
-          slug="best-seller"
-          redirectTo="/collections/best-seller"
-          large={true}
-        />
-
-        <ProductCollection
-          products={featuredProducts}
-          title="Our featured"
-          slug="featured"
-          redirectTo="/collections/featured"
-          large={true}
-        />
-        <CategorySection categories={categories} />
-        <ReviewSection />
-        <BrandSection brands={brands} />
-      </div>
-    </main>
-  );
-}
 
 export const getStaticProps = async () => {
   try {
@@ -183,8 +103,3 @@ export const getStaticProps = async () => {
     };
   }
 };
-
-V1Page.showStickyCheckout = true;
-V1Page.showTopRunner = true;
-V1Page.couponBanner = true;
-export default V1Page;
