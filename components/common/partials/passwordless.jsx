57529fb19c277ebe6762da4069a7b521d581fadb
bhagyashreebothra <bhagyashree@devxconsultancy.com>
2023-07-06 15:16:01 +0530

    WOWBC-359 - Added OTP Auto read

--- components/common/partials/passwordless.jsx
+++ components/common/partials/passwordless.jsx
@@ -180,6 +180,38 @@ function Passwordless({
   }, [isOpen, confirmSignUp]);
 
   useEffect(() => {
+    if ("OTPCredential" in window) {
+      window.addEventListener("DOMContentLoaded", (e) => {
+        const input = document.querySelector('input[autocomplete="one-time-code"]');
+        if (!input) return;
+        const ac = new AbortController();
+        const form = input.closest("form");
+        if (form) {
+          form.addEventListener("submit", (e) => {
+            ac.abort();
+          });
+        }
+        navigator.credentials
+          .get({
+            otp: { transport: ["sms"] },
+            signal: ac.signal,
+          })
+          .then((otp) => {
+            setState({
+              ...state,
+              confirmationCode: otp.code.split("")
+            });
+            // input.value = otp.code;
+            if (form) form.submit();
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      });
+    }
+  }, []);
+
+  useEffect(() => {
     if (state.confirmationCode.join("").length === 6) {
       handleConfirmSignUp();
     }
