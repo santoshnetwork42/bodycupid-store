f4c52ba7b485743fd9dbfe57214a55700e8ba7b9
bhagyashree-bothra <138487306+bhagyashree-bothra@users.noreply.github.com>
2023-07-12 18:23:51 +0530

    Update addressForm.jsx

--- components/common/addressForm.jsx
+++ components/common/addressForm.jsx
@@ -21,7 +21,7 @@ const AddressForm = (props) => {
     email: email || null,
     phone: phone || "",
     address: "",
-    state: "AN",
+    state: "GJ",
     city: "",
     pinCode: "",
     landmark: "",
