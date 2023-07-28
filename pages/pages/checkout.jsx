830b51eb65a2079e66fb2ff3f202b70916e17e61
bhagyashree-bothra <bhagyashree@devxconsultancy.com>
2023-07-28 23:54:26 +0530

    lint

--- pages/pages/checkout.jsx
+++ pages/pages/checkout.jsx
@@ -342,7 +342,7 @@ function Checkout(props) {
             products: productIds,
             shippingAddress: restAddress,
             billingAddress: restAddress,
-            couponCode: appliedCoupon?.code, 
+            couponCode: appliedCoupon?.code,
             storeId: STORE_ID,
             paymentType: payMethod,
             ...metadata,
