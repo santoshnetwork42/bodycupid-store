c795111167b1e55d737eb2edb340a2312b70fb18
Brijesh Agarwal <brijesh.agarwal26@gmail.com>
2023-08-18 19:31:15 +0530

    updated menu

--- utils/contexts/navbar.js
+++ utils/contexts/navbar.js
@@ -197,6 +197,24 @@ export const useMenu = () => {
     slug: "raksha-bandhan-gifts",
   });
 
+  menu.push({
+    label: "Offers",
+    link: "",
+    slug: "offers",
+    subMenu: [
+      {
+        label: "BUY 3 @ 599",
+        link: "/collections/special-bundle-offer",
+        slug: "special-bundle-offer",
+      },
+      {
+        label: "BUY 3 @ 1099",
+        link: "/collections/fragrance-bundle-offer",
+        slug: "fragrance-bundle-offer",
+      },
+    ],
+  });
+
   return menu;
 };
 
