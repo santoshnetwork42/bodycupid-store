8d9cdaaa6ba90ee7e8f7c7e9ff7644ff57fd2fa6
Brijesh Agarwal <brijesh.agarwal26@gmail.com>
2023-09-02 11:03:27 +0530

    fix

--- utils/contexts/ab.js
+++ utils/contexts/ab.js
@@ -1,24 +1,24 @@
-import React, { createContext, useState, useContext, useEffect } from "react";
-import Cookie from "js-cookie";
-import { useRouter } from "next/router";
+import React, { createContext } from "react";
+// import Cookie from "js-cookie";
+// import { useRouter } from "next/router";
 
-import { STORE_PREFIX } from "~/config";
+// import { STORE_PREFIX } from "~/config";
 
-const CART_AB = `${STORE_PREFIX}_CARTAB`; 
+// const CART_AB = `${STORE_PREFIX}_CARTAB`;
 
 export const ABContext = createContext();
 
 function ABProvider({ children }) {
-  const router = useRouter();
+  // const router = useRouter();
 
-  const { query } = router;
+  // const { query } = router;
 
   return <ABContext.Provider>{children}</ABContext.Provider>;
 }
 
-export const useCartAB = () => {
-  const { cartAB } = useContext(ABContext);
-  return cartAB;
-};
+// export const useCartAB = () => {
+//   const { cartAB } = useContext(ABContext);
+//   return cartAB;
+// };
 
 export default ABProvider;
