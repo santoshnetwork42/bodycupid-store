7b23a34ccb31d7cd9dcc2409c660b2614eef6d46
bhagyashree-bothra <bhagyashree@devxconsultancy.com>
2023-09-08 12:15:52 +0530

    ttl

--- store/saga/shopping-cart.js
+++ store/saga/shopping-cart.js
@@ -9,75 +9,72 @@ import { errorHandler } from "~/utils/errorHandler";
 
 export function* cartSaga() {
   yield takeEvery(
-    [...Object.keys(actionTypes), userActionTypes.SET_USER],
-    function* saga(e) {
-      if (
-        [
-          actionTypes.ADD_TO_CART,
-          actionTypes.REMOVE_FROM_CART,
-          actionTypes.APPLY_COUPONS,
-          actionTypes.REMOVE_COUPON,
-          actionTypes.UPDATE_CART,
-          userActionTypes.SET_USER,
-        ].includes(e.type)
-      ) {
-        try {
-          const { user, cart } = yield select();
-          let { coupon, data: cartResponse } = cart;
-          const { data: userData } = user;
+    [
+      actionTypes.ADD_TO_CART,
+      actionTypes.REMOVE_FROM_CART,
+      actionTypes.APPLY_COUPONS,
+      actionTypes.REMOVE_COUPON,
+      actionTypes.UPDATE_CART,
+      userActionTypes.SET_USER,
+    ],
+    function* saga() {
+      try {
+        console.log("====saga");
+        const { user, cart } = yield select();
+        let { coupon, data: cartResponse } = cart;
+        const { data: userData } = user;
 
-          if (userData) {
-            const { id: couponId } = coupon || {};
+        if (userData) {
+          const { id: couponId } = coupon || {};
 
-            const data = cartResponse.map(
-              ({
-                id,
-                variantId,
-                qty,
-                shoppingCartProductId,
-                cartItemSource,
-              }) => ({
-                shoppingCartProductId,
-                productId: id,
-                variantId,
-                quantity: qty,
-                source: cartItemSource || null,
-              })
-            );
+          const data = cartResponse.map(
+            ({
+              id,
+              variantId,
+              qty,
+              shoppingCartProductId,
+              cartItemSource,
+            }) => ({
+              shoppingCartProductId,
+              productId: id,
+              variantId,
+              quantity: qty,
+              source: cartItemSource || null,
+            })
+          );
 
-            const {
-              data: { handleStoreShoppingCart: response },
-            } = yield call([API, API.graphql], {
-              query: handleStoreShoppingCart,
-              variables: {
-                input: {
-                  couponCodeId: couponId || null,
-                  storeId: STORE_ID,
-                  data,
-                },
+          const {
+            data: { handleStoreShoppingCart: response },
+          } = yield call([API, API.graphql], {
+            query: handleStoreShoppingCart,
+            variables: {
+              input: {
+                couponCodeId: couponId || null,
+                storeId: STORE_ID,
+                data,
               },
-              authMode: "AMAZON_COGNITO_USER_POOLS",
-            });
+            },
+            authMode: "AMAZON_COGNITO_USER_POOLS",
+          });
 
-            if (response) {
-              const { shoppingcartProducts, ...cart } = response;
-              const products = cartResponse.map((obj, index) => ({
-                ...obj,
-                shoppingCartProductId: shoppingcartProducts.items[index].id,
-              }));
-              yield put({
-                type: actionTypes.SET_CART,
-                payload: {
-                  products,
-                },
-              });
-            } else {
-              yield put({ type: actionTypes.REFRESH_CART });
-            }
+          if (response) {
+            const { shoppingcartProducts, ...cart } = response;
+            const products = cartResponse.map((obj, index) => ({
+              ...obj,
+              shoppingCartProductId: shoppingcartProducts.items[index].id,
+            }));
+            yield put({
+              type: actionTypes.SET_CART,
+              payload: {
+                products,
+              },
+            });
+          } else {
+            yield put({ type: actionTypes.REFRESH_CART });
           }
-        } catch (e) {
-          errorHandler(e);
         }
+      } catch (e) {
+        errorHandler(e);
       }
     }
   );
