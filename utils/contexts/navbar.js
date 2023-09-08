aba44dd9ad47d4919b2b7eb458e4a66f49b2aae3
Brijesh Agarwal <brijesh.agarwal26@gmail.com>
2023-09-08 16:34:43 +0530

    fix

--- utils/contexts/navbar.js
+++ utils/contexts/navbar.js
@@ -202,11 +202,12 @@ export const useMenu = () => {
       slug: "combos-and-gifts",
     });
 
-  menu.push({
-    label: "Clearance Sale",
-    link: `/collections/clearance-sale`,
-    slug: "clearance-sale",
-  });
+    menu.push({
+      label: "Clearance Sale",
+      link: `/collections/clearance-sale`,
+      slug: "clearance-sale",
+    });
+  }
 
   return menu;
 };
