e51e7666c235bd54de0b0ff44052b6c1f44a0369
Nishita Pandya <133861460+nishitaDevX@users.noreply.github.com>
2023-10-02 17:22:38 +0530

    Update coupon.jsx

--- components/features/coupon.jsx
+++ components/features/coupon.jsx
@@ -313,7 +313,7 @@ function Coupon(props) {
         <div className="slider-header p-0 coupon-heading">
           <div className="d-flex lh-1 align-items-center">
             <ALink href="#" onClick={closeSlider}>
-              <LeftAngle />
+              <LeftAngle  />
             </ALink>
             <div className="cart-title ml-2">
               COUPONS ({featuredCoupons.length})
