88aabbe135bca0e185a3e84e98f61710296e08fb
nishitaDevX <nishita@devxconsultancy.com>
2023-10-07 13:36:35 +0530

    added privacy policy on otp

--- components/common/partials/passwordless.jsx
+++ components/common/partials/passwordless.jsx
@@ -336,6 +336,22 @@ function Passwordless({
                               Get OTP
                               {loading && <div className="spin-loader ml-2" />}
                             </button>
+                            <p>
+                              By clicking Submit, you agree to our{" "}
+                              <ALink
+                                href="/policies/terms-of-service"
+                                className="link-text"
+                              >
+                                Terms and Conditions
+                              </ALink>{" "}
+                              and{" "}
+                              <ALink
+                                href="/policies/privacy-policy"
+                                className="link-text"
+                              >
+                                Privacy Policy
+                              </ALink>
+                            </p>
                           </form>
                         )}
                         {/* <div className="form-choice text-center">
