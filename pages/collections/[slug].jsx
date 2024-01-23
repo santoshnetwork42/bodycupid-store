800f5005ac77ab3923780a20446ee9c43e534e20
nishitaDevX <nishita@devxconsultancy.com>
2024-01-23 17:06:25 +0530

    ttm

--- pages/collections/[slug].jsx
+++ pages/collections/[slug].jsx
@@ -199,7 +199,7 @@ export const getStaticProps = async (context) => {
             image: getPublicImageURL(imageUrl),
           },
         },
-        revalidate: 120,
+        revalidate: 1800,
       };
     }
 
@@ -276,7 +276,7 @@ export const getStaticProps = async (context) => {
             image: getPublicImageURL(imageUrl),
           },
         },
-        revalidate: 120,
+        revalidate: 1800,
       };
     }
 
