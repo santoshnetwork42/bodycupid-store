3894187976d2751e73714214070800ffbf9807e2
Nishita Pandya <133861460+nishitaDevX@users.noreply.github.com>
2024-02-28 10:47:54 +0530

    Update Affise.jsx

--- components/scripts/Affise/Affise.jsx
+++ components/scripts/Affise/Affise.jsx
@@ -14,7 +14,6 @@ function Affise() {
         });
         return params;
       };
-
       // Function to store query parameters in local storage
       var storeCKInLocalStorage = function (params) {
         // Remove specific items from local storage if they are not present in 'params'
