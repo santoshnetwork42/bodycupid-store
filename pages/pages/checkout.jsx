7581115e95e4bda06b13145e6bd892f9bc8a485b
nishitaDevX <nishita@devxconsultancy.com>
2024-03-07 21:57:24 +0530

    solved guest checkout

--- pages/pages/checkout.jsx
+++ pages/pages/checkout.jsx
@@ -167,7 +167,7 @@ function Checkout(props) {
     try {
       e.preventDefault();
 
-      if (!user || !user.isActive) {
+      if (!user || !user.isActive || !guestCheckout) {
         emptyCart();
         router.replace("/pages/order-failed");
         return;
