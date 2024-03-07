c227549f84dd95f9e624ea4d0f2e49c650550931
Nishita Pandya <133861460+nishitaDevX@users.noreply.github.com>
2024-03-07 11:54:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
 const { withSentryConfig } = require("@sentry/nextjs");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
