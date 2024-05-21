6d508e740a2ba05342234b5a1a8ab6132e0ee132
vasuDevX <vasu.chapadia@devxconsultancy.com>
2024-05-21 19:09:58 +0530

    fix: image optimization

--- utils/getPublicImageUrl.js
+++ utils/getPublicImageUrl.js
@@ -2,7 +2,10 @@ import { MEDIA_BASE_URL } from "~/config";
 
 export const getPublicImageURL = (key, resize, quality = 75) => {
   if (resize) {
-    return `https://${MEDIA_BASE_URL}/public/${key}?w=${resize}&q=${quality}`;
+    return `https://${MEDIA_BASE_URL}/public/${key}?w=${resize}&q=${Math.max(
+      75,
+      quality
+    )}&f=webp`;
   }
-  return `https://${MEDIA_BASE_URL}/public/${key}`;
+  return `https://${MEDIA_BASE_URL}/public/${key}?f=webp`;
 };
