a6b296d635d7272dab2cbabdb9474dcd1608dde2
Shruti <shruti@devxconsultancy.com>
2024-06-04 14:22:37 +0530

    fix: daily timer

--- components/common/countDownTimer.jsx
+++ components/common/countDownTimer.jsx
@@ -1,19 +1,22 @@
 import { useConfiguration } from "@wow-star/utils";
 import dayjs from "dayjs";
+import isBetween from "dayjs/plugin/isBetween";
 import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
 import {
+  IS_DAILY_TIMER,
   TIMER_BG_COLOR,
   TIMER_COLOR,
   TIMER_DESCRIPTION,
   TIMER_ENABLED,
   TIMER_END_TIME,
   TIMER_START_TIME,
-  IS_DAILY_TIMER,
   TIMER_TTILE,
 } from "~/constant";
 import { countTimeFromSeconds } from "~/utils/countDownTimeCalc";
+
+dayjs.extend(isBetween);
 
 const Timer = ({ displayTimer }) => {
   const timerEnabled = useConfiguration(TIMER_ENABLED, false);
@@ -21,11 +24,9 @@ const Timer = ({ displayTimer }) => {
   const timerDescription = useConfiguration(TIMER_DESCRIPTION, "");
   const timerColor = useConfiguration(TIMER_COLOR, "#FFFFFF");
   const timerBgColor = useConfiguration(TIMER_BG_COLOR, "#2E8B57");
-  const startDate = useConfiguration(TIMER_START_TIME);
-  const endDate = useConfiguration(TIMER_END_TIME);
+  const startDateConfig = useConfiguration(TIMER_START_TIME);
+  const endDateConfig = useConfiguration(TIMER_END_TIME);
   const isDaily = useConfiguration(IS_DAILY_TIMER, false);
-
-  const [showTimer, setShowTimer] = useState(timerEnabled);
 
   const [timeLeft, setTimeLeft] = useState({
     days: "00",
@@ -35,42 +36,77 @@ const Timer = ({ displayTimer }) => {
   });
 
   useEffect(() => {
-    if (endDate) {
-      const interval = setInterval(() => {
-        const currentTime = dayjs();
-        const start = dayjs(startDate);
-        const end = dayjs(endDate);
+    if (!timerEnabled) return;
 
-        if (currentTime.isAfter(start) && currentTime.isBefore(end)) {
-          const timeInSeconds = dayjs(end).diff(dayjs());
+    const startTime = dayjs(startDateConfig).format("HH:mm:ss");
+    const endTime = dayjs(endDateConfig).format("HH:mm:ss");
 
-          const { days, hours, minutes, seconds } =
-            countTimeFromSeconds(timeInSeconds);
-          setTimeLeft({
-            days: isDaily ? "00" : days,
-            hours,
-            minutes,
-            seconds,
-          });
-          setShowTimer(timerEnabled);
+    const updateTimeLeft = () => {
+      const now = dayjs();
+      const currentTime = now.format("HH:mm:ss");
+
+      if (now.isBetween(startDateConfig, endDateConfig)) {
+        if (isDaily) {
+          if (currentTime >= startTime && currentTime <= endTime) {
+            const durationInSeconds = dayjs(endDateConfig).diff(now);
+            if (durationInSeconds > 0) {
+              const { days, hours, minutes, seconds } =
+                countTimeFromSeconds(durationInSeconds);
+              setTimeLeft({
+                days: isDaily ? "00" : days,
+                hours,
+                minutes,
+                seconds,
+              });
+            } else {
+              resetTimeLeft();
+            }
+          } else {
+            resetTimeLeft();
+          }
         } else {
-          setTimeLeft({
-            days: "00",
-            hours: "00",
-            minutes: "00",
-            seconds: "00",
-          });
-          setShowTimer(false);
+          const durationInSeconds = dayjs(endDateConfig).diff(now);
+          if (durationInSeconds > 0) {
+            const { days, hours, minutes, seconds } =
+              countTimeFromSeconds(durationInSeconds);
+            setTimeLeft({
+              days,
+              hours,
+              minutes,
+              seconds,
+            });
+          } else {
+            resetTimeLeft();
+          }
         }
-      }, 1000);
-      return () => clearInterval(interval);
-    }
-  }, [endDate]);
+      } else {
+        resetTimeLeft();
+      }
+    };
+    const interval = setInterval(updateTimeLeft, 1000);
 
-  if (!timerEnabled || !showTimer) return <></>;
+    return () => clearInterval(interval);
+  }, [timerEnabled, startDateConfig, endDateConfig]);
+
+  const resetTimeLeft = () => {
+    setTimeLeft({
+      days: "00",
+      hours: "00",
+      minutes: "00",
+      seconds: "00",
+    });
+  };
+
+  if (
+    !timerEnabled ||
+    !timeLeft ||
+    Object.values(timeLeft).every((val) => val === "00")
+  )
+    return <></>;
 
   return (
-    displayTimer && (
+    displayTimer &&
+    timerEnabled && (
       <div
         className="announcement d-flex-col align-items-center pb-2 pt-2"
         style={{ background: timerBgColor, color: timerColor }}
