acd2aadb7e55305593ea8952f8fd72d1657fb334
piyushjain <piyush@devxconsultancy.com>
2024-07-11 16:26:47 +0530

    hide error handler

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -4,11 +4,11 @@ import { alertToaster } from "./popupHelper";
 export const errorHandler = (error) => {
   if (typeof error === "string" || error?.message) {
     console.log(error?.message || error);
-    alertToaster(error?.message || error, "error");
-    Sentry.captureException(error?.message || error);
+    // alertToaster(error?.message || error, "error");
+    // Sentry.captureException(error?.message || error);
   } else {
     console.log(JSON.stringify(error));
-    alertToaster("Something went wrong!", "error");
-    Sentry.captureException(JSON.stringify(error));
+    // alertToaster("Something went wrong!", "error");
+    // Sentry.captureException(JSON.stringify(error));
   }
 };
