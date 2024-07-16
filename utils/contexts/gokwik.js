e98a23c481a5efe2c0ccf223aebb0b98d3b5ef94
vasuDevX <vasu.chapadia@devxconsultancy.com>
2024-07-16 13:22:33 +0530

    fix: revert aws export

--- utils/contexts/gokwik.js
+++ utils/contexts/gokwik.js
@@ -242,7 +242,7 @@ function GoKwikProvider({
     });
 
     gokwikSdk.on("user-login-successful", async (event) => {
-      console.log("user-login-successful>>>", event);
+      logger.log("user-login-successful>>>", event);
       await startCheckout("GOKWIK");
       await manageUserAuthEvent(event.phone_no, event.user_token);
     });
