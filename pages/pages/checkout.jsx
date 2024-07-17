80c171722bafd48bc994e40c7140475fdd705e2f
vasuDevX <vasu.chapadia@devxconsultancy.com>
2024-07-17 12:39:53 +0530

    fix: updates cta ppcod comms

--- pages/pages/checkout.jsx
+++ pages/pages/checkout.jsx
@@ -752,7 +752,9 @@ function Checkout(props) {
                                   : ppcodEnabled && ppcodAmount
                                   ? `Pay ₹${toDecimal(
                                       ppcodAmount
-                                    )} now non refundable and remaining on delivery.`
+                                    )} now (non-refundable). Rest ₹${
+                                      codGrandTotal - ppcodAmount
+                                    } on delivery.`
                                   : "Pay using Cash on Delivery."
                               }
                               disabled={codCouponDisabled || isMaxCODDisabled}
