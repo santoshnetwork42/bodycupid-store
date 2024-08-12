f6f2a8fefbb97a526af83aa0b9c264cfa58ca5b3
piyushjain <piyush@devxconsultancy.com>
2024-08-12 13:21:47 +0530

    vercel deploy

--- components/partials/product/detail/detail-one.jsx
+++ components/partials/product/detail/detail-one.jsx
@@ -202,6 +202,10 @@ function DetailOne(props) {
         <ProductBreadcrumbs {...product} />
       </div>
 
+      {product?.collections?.includes("bundle-offer") && (
+        <div className="product-label label-pdp">BUY 6 @ 999</div>
+      )}
+
       <h2 className="detail-product-name text-uppercase">{product.title}</h2>
 
       {/* {!!product?.tags && (
