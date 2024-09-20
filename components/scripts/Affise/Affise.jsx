893a23f9bd14019d5af2a68b54f98805c4dc6c4c
piyushjain <piyush@devxconsultancy.com>
2024-09-20 12:31:53 +0530

    vercel deploy

--- components/scripts/Affise/Affise.jsx
+++ components/scripts/Affise/Affise.jsx
@@ -53,7 +53,6 @@ function Affise() {
               localStorage.setItem("bw_" + key, params[key]);
             }
           }
-          
 
           // Store the current timestamp in local storage
           var currentTimestamp = Math.floor(Date.now() / 1000); // Convert milliseconds to seconds
