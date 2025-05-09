e54a9de1305c6133c35976a47738ac86caa64533
JayDevx <jay@devxconsultancy.com>
2025-05-09 09:12:48 +0530

    prod backsync

--- aws-exports.js
+++ aws-exports.js
@@ -1,30 +1,30 @@
 const awsmobile = {
   aws_project_region: "ap-south-1",
-  aws_mobile_analytics_app_id: "68f611fe536b4251b61ebda99da40e55",
+  aws_mobile_analytics_app_id: "ca8ef46d368243e3b4c129f929181d5d",
   aws_mobile_analytics_app_region: "ap-south-1",
   Analytics: {
     AWSPinpoint: {
-      appId: "68f611fe536b4251b61ebda99da40e55",
+      appId: "ca8ef46d368243e3b4c129f929181d5d",
       region: "ap-south-1",
     },
   },
   aws_appsync_graphqlEndpoint:
-    "https://i3t5repbizh37gtuq3im2tr2aa.appsync-api.ap-south-1.amazonaws.com/graphql",
+    "https://k4ah3qum7rbtjns24dtw3kp4ee.appsync-api.ap-south-1.amazonaws.com/graphql",
   aws_appsync_region: "ap-south-1",
   aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: "da2-vpj5of2bkja2jfrq75scl5a3sm",
+  aws_appsync_apiKey: "da2-oirunkudqbh2bayylafkepfsju",
   aws_cloud_logic_custom: [
     {
-      name: "wow-uat-rest-api",
-      endpoint: "https://5idhmv6fzb.execute-api.ap-south-1.amazonaws.com/uat",
+      name: "wow-prod-rest-api",
+      endpoint: "https://pz6ifs3lr9.execute-api.ap-south-1.amazonaws.com/prod",
       region: "ap-south-1",
     },
   ],
   aws_cognito_identity_pool_id:
-    "ap-south-1:806cebad-c8c3-4455-ba87-11f569eb6520",
+    "ap-south-1:33117795-fada-45fd-8e18-777051a746f6",
   aws_cognito_region: "ap-south-1",
-  aws_user_pools_id: "ap-south-1_38nNwRAiO",
-  aws_user_pools_web_client_id: "7fapa76bd6kbmlimkd6stnsm4t",
+  aws_user_pools_id: "ap-south-1_XMkWXELxe",
+  aws_user_pools_web_client_id: "42l9t4l4au9jsauhoqio5lleeq",
   oauth: {},
   aws_cognito_username_attributes: ["PHONE_NUMBER"],
   aws_cognito_social_providers: [],
@@ -36,7 +36,7 @@ const awsmobile = {
     passwordPolicyCharacters: [],
   },
   aws_cognito_verification_mechanisms: ["PHONE_NUMBER"],
-  aws_user_files_s3_bucket: "wow-uat-media-bucket",
+  aws_user_files_s3_bucket: "wow-prod-media-bucket",
   aws_user_files_s3_bucket_region: "ap-south-1",
 };
 export default awsmobile;
